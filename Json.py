811c75cbfd8e30085b23f73d91ca89396767bfd5
Diamondd1111 <DButler@sea.yearup.org>
2024-12-12 14:14:24 -0800

    Update Json.py

--- Json.py
+++ Json.py
@@ -20,7 +20,7 @@ with open('data1.json','w') as json_file:
     json.dump(data1, json_file, indent=4)
 
 
-#This code lets us know the code has been written into data1.json
+#Run the code to create data1.json file
 print("Data has been written to data1.json")
 
 #Read json file
